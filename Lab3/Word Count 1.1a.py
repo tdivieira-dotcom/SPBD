10683ce8ef4f4c9914eaa06552bb0e68bf8a1a5c
tdivieira-dotcom <tdi.vieira@campus.fct.unl.pt>
2025-10-14 18:27:36 +0100

    Update Word Count 1.1a.py

--- Lab3/Word Count 1.1a.py
+++ Lab3/Word Count 1.1a.py
@@ -11,17 +11,17 @@ spark = SparkSession \
 sc = spark.sparkContext
 
 try:
-    //colocar cada linha a minusculas, sem acentos nem quebras de linha
+    #colocar cada linha a minusculas, sem acentos nem quebras de linha
   lines = sc.textFile('os_maias.txt') \
             .filter(lambda line: len(line) > 0) \
             .map(lambda line: line.strip()) \
             .map(lambda line: unicode(line).lower()) \
             .map(lambda line: line.translate(str.maketrans('','', string.punctuation+'«»')))
 
-   //dividir cada linha em words, criar par {word,1} e agrupar por keys (fazendo a sua soma)
-  words = lines.flatMap(lambda line : line.split()) \  //lambda é uma função que a cada linha divide em words
-            .map(lambda word: (word,1))\     //lambda é uma função que a cada word cria um par {word,1}
-            .reduceByKey(lambda a,b: a+b)   //lambda é uma função que a cada elemento a cada key soma os seus valores
+   #dividir cada linha em words, criar par {word,1} e agrupar por keys (fazendo a sua soma)
+  words = lines.flatMap(lambda line : line.split()) \  #lambda é uma função que a cada linha divide em words
+            .map(lambda word: (word,1))\     #lambda é uma função que a cada word cria um par {word,1}
+            .reduceByKey(lambda a,b: a+b)   #lambda é uma função que a cada elemento a cada key soma os seus valores
 
   for word, count in words.take(10):
     print(f"{word}:{count}")
