e459f2d840771d0473c633e75ed50944f20dae23
tdivieira-dotcom <tdi.vieira@campus.fct.unl.pt>
2025-10-14 16:40:10 +0100

    Update Weblog Analysis 3.py

--- Lab1/Weblog Analysis 3.py
+++ Lab1/Weblog Analysis 3.py
@@ -1,1 +1,19 @@
+from mrjob.job import MRJob, MRStep
 
+class MRWebLogStats2c_V1(MRJob):
+
+  def mapper(self, _, line):
+      tokens = line.split('\t')
+      if len(tokens) == 6:   # make sure the line is complete
+            timestamp = parts[0] //token da data
+            ip = tokens[1]       //token do ip
+            URL = tokens[4]      //token do URL
+            time_interval_10s = timestamp[0:18] //dentro da data só queremos até aos segundos
+            yield "{}-{}".format(time_interval_10s, URL), ip   //cada URL e segundo fica com o ip de que foi acedido
+
+  def reducer(self, interval_URL, ips):
+            unique_ip= set(ips)
+            yield interval_URL, unique_ip
+  
+if __name__ == '__main__':
+    MRWebLogStats2c_V1.run()
