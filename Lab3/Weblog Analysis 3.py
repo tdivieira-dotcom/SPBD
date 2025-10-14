06ae4fb2a4cb7b0a49372b2238a02acc86dc7255
tdivieira-dotcom <tdi.vieira@campus.fct.unl.pt>
2025-10-14 20:57:35 +0100

    Update Weblog Analysis 3.py

--- Lab3/Weblog Analysis 3.py
+++ Lab3/Weblog Analysis 3.py
@@ -1,1 +1,26 @@
 
+#@title Weblog Analysis3
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession \
+    .builder \
+    .appName("Weblog Analysis3") \
+    .getOrCreate()
+
+sc = spark.sparkContext
+
+try:
+  lines = sc.textFile('web.log') \
+            .map(lambda line: line.split('\t')) \
+            .filter( lambda line: len(line) == 6)
+
+
+  tuple = lines.map(lambda line: (line[0][0:18],line[4],line[1]))\      #crias tuples ip,1 
+          .count() #remove os ips duplicados
+
+ print(f"{number_unique_ips}")
+
+except Exception as e:
+  print(e)
