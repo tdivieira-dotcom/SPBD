e6f38f955aa73820868fd1fee7417c7b31518c9f
tdivieira-dotcom <tdi.vieira@campus.fct.unl.pt>
2025-10-14 10:44:35 +0100

    Update 1.py

--- Weblog Analysis/1.py
+++ Weblog Analysis/1.py
@@ -1,19 +1,18 @@
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 
 class MRUniqueIPs(MRJob):
 
     def mapper(self, _, line):
-        # Ignora linhas vazias ou cabeçalho
-        tokens = line.strip().split('\t')
-
-        ip = parts[1]  
+        tokens = line.split('\t')
+        ip = tokens[1]  
         yield ip, 1  # Cada IP lido fica com 1
 
-    def reducer(self, ip, counts):
-        yield "total_unique_ips", 1  # Cada IP diferente fica com [total_unique_ips,1], [total_unique_ips,1], ...
+    def reducer(self, ip, unique):
+        yield ip, 1  # Cada key fica com valor 1(em vez de somar)
 
     def reducer_sum(self, key, values):
-        yield key, sum(values)    # Somamos todos os "1" para obter o total final de IPs únicos
+        yield "total_unique_ips", sum(values) # Criamos esta key que tem como value a soma de todos os 1 relativo a cada ip único
 
 
     # Define a sequência de passos (Map → Reduce → Reduce)
