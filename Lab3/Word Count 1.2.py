eb2fb2d73fb241820d2836cb97eb0eaa7512d95a
tdivieira-dotcom <tdi.vieira@campus.fct.unl.pt>
2025-10-14 19:22:25 +0100

    Update Word Count 1.2.py

--- Lab3/Word Count 1.2.py
+++ Lab3/Word Count 1.2.py
@@ -22,12 +22,21 @@ try:
             .map(lambda word: (word,1))\
             .reduceByKey( lambda a,b: a+b)
 
-  
-  words_frequency=words.sortBy(lambda x: x[1], ascending=False) #false- da maior frequência para a menor, o x é o RDD com a tupla por isso x[1] ordena pela frequência
-  index_words_frequency=words_frequency.zipWithIndex()
-  
-  for word, count in index_words_frequency.take(10):
-    print(f"{word}:{count}")
+  # Top-10 por partição (pré-seleção local)
+ top10_per_partition = words.mapPartitions( #isto é aplicado a grandes partições de tuplas (em vez de fazermos o sorting de 1000 tuplas diretamente, por ex.)
+    lambda partition: sorted(partition, key=lambda k: k[1], reverse=True)[:10] #ordenamos as tuplas por frequencia e apenas retiramos as 10 mais frequentes([:10])
+)
+
+  top10_global = top10_per_partition.sortBy(lambda x: x[1], ascending=False)\ #false- da maior frequência para a menor, o x é o RDD com a tupla por isso x[1] ordena pela frequência
+  .zipWithIndex()\                                                          #As palavras mais frequentes têm indice a começar em 0
+  #O filter faz com que apenas tenhamos os 10 tuplos mais frequentes pois apenas queremos até ao indice(x[1]) menor do que 10
+  #O map faz com que eliminemos a parte do indice da tupla pois só queremos o x[0] (word, count)
+  .filter(lambda x: x[1] < 10)\
+  .map(lambda x: x[0]) 
+    
+print("Top-10 Most Frequent Words:")
+for word, count in top10_global.collect():
+    print(f"{word}: {count}")
 
 except Exception as e:
   print(e)
