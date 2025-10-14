e6d0ca3bb9ecc81fea4136ac6cc5ee177233864b
tdivieira-dotcom <tdi.vieira@campus.fct.unl.pt>
2025-10-14 18:28:53 +0100

    Update Weblog Analysis 3.py

--- Lab1/Weblog Analysis 3.py
+++ Lab1/Weblog Analysis 3.py
@@ -5,14 +5,14 @@ class MRWebLogStats2c_V1(MRJob):
   def mapper(self, _, line):
       tokens = line.split('\t')
       if len(tokens) == 6:   # make sure the line is complete
-            timestamp = parts[0] //token da data
-            ip = tokens[1]       //token do ip
-            URL = tokens[4]      //token do URL
-            time_interval_10s = timestamp[0:18] //dentro da data só queremos até aos segundos
-            //inverted index- cada time e URL ficam com os ips em vez de cada IP ficar com URL
-            yield "{}-{}".format(time_interval_10s, URL), ip   //cada URL e segundo fica com o ip de que foi acedido
+            timestamp = parts[0] #token da data
+            ip = tokens[1]       #token do ip
+            URL = tokens[4]      #token do URL
+            time_interval_10s = timestamp[0:18] #dentro da data só queremos até aos segundos
+            #inverted index- cada time e URL ficam com os ips em vez de cada IP ficar com URL
+            yield "{}-{}".format(time_interval_10s, URL), ip   #cada URL e segundo fica com o ip de que foi acedido
 
-  def reducer(self, interval_URL, ips):  //interval_URL é um tuple {time_interval_10s, URL}
+  def reducer(self, interval_URL, ips):  #interval_URL é um tuple {time_interval_10s, URL}
             unique_ip= set(ips)
             yield interval_URL, unique_ip
   
