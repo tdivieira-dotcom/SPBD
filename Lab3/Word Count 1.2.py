fbbf5d67db3e560c18be8ff461177126712f7b2d
tdivieira-dotcom <tdi.vieira@campus.fct.unl.pt>
2025-10-14 18:49:02 +0100

    Update Word Count 1.2.py

--- Lab3/Word Count 1.2.py
+++ Lab3/Word Count 1.2.py
@@ -1,1 +1,34 @@
 
+#@title WordFrequecy Spark Core 1.2)
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark = SparkSession \
+    .builder \
+    .appName("WordFrequency1.2") \
+    .getOrCreate()
+
+sc = spark.sparkContext
+
+try:
+  lines = sc.textFile('os_maias.txt') \
+            .filter(lambda line: len(line) > 0) \
+            .map(lambda line: line.strip()) \
+            .map(lambda line: unicode(line).lower()) \
+            .map(lambda line: line.translate(str.maketrans('','', string.punctuation+'«»')))
+
+  words = lines.flatMap(lambda line : line.split()) \
+            .map(lambda word: (word,1))\
+            .reduceByKey( lambda a,b: a+b)
+
+  
+  words_frequency=words.sortBy(lambda x: x[1], ascending=False) #false- da maior frequência para a menor, o x é o RDD com a tupla por isso x[1] ordena pela frequência
+  index_words_frequency=words_frequency.zipWithIndex()
+  
+  for word, count in index_words_frequency.take(10):
+    print(f"{word}:{count}")
+
+except Exception as e:
+  print(e)
+
