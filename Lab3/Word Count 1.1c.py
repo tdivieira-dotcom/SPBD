266f8e74dfeb5894f0ebd5a74e74448f04db493c
tdivieira-dotcom <tdi.vieira@campus.fct.unl.pt>
2025-10-14 18:38:50 +0100

    Update Word Count 1.1c.py

--- Lab3/Word Count 1.1c.py
+++ Lab3/Word Count 1.1c.py
@@ -15,14 +15,14 @@ try:
             .filter(lambda line: len(line) > 0) \
             .map(lambda line: line.strip()) \
             .map(lambda line: unicode(line).lower()) \
-            .map(lambda line: line.translate(str.maketrans('','', string.punctuation+'')))
+            .map(lambda line: line.translate(str.maketrans('','', string.punctuation+'«»')))
 
   words = lines.flatMap(lambda line : line.split()) \
             .map(lambda word: (word,1))\
             .reduceByKey( lambda a,b: a+b)
 
   
-  sorted_words=words.sortedBy(ascending=False, keyfunc =lambda t: t[1])
+  words_frequency=words.sortBy(lambda x: x[1], ascending=False) #false- da maior frequência para a menor, o x é o RDD com a tupla por isso x[1] ordena pela frequência
   for w in sorted_words.collect(10):
     print(w)
 
