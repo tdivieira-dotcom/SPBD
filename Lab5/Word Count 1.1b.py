49e7c8f397f1390adc7be478d9f7daf700ba09e3
tdivieira-dotcom <tdi.vieira@campus.fct.unl.pt>
2025-11-18 19:36:01 +0000

    Create Word Count 1.1b.py

---
+++ Lab5/Word Count 1.1b.py
@@ -0,0 +1,25 @@
+#@title 1.1b)
+from pyspark.sql import *
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.master('local[*]').appName('wordfrequency1.1b)').getOrCreate()
+sc = spark.sparkContext
+
+try :
+  lines = sc.textFile('os_maias.txt') \
+            .filter( lambda line : len(line) > 1 )
+
+  structured_lines = lines.map( lambda line : Row( line = line, listOfWords = line.split(' ') ) )
+
+  wordsOfLine = spark.createDataFrame( structured_lines )
+  
+  words = wordsOfLine.withColumn('words', explode('listOfWords')) #cria nova coluna words que "explode" a coluna listOfWords criando várias linhas para cada palavra presente na linha
+  words = words.drop('line', 'listOfWords') #elimina as colunas line e list of words, ficando apenas a coluna words com uma palavra por linha
+  frequencies = words.groupBy('words').count() #juntar as keys e somar a sua frequencia
+  alphabetic_order= frequencies.orderBy('words', ascending= 'false')
+  alphabetic_order.show()
+
+except Exception as err:
+  print(err)
+  sc.stop()
