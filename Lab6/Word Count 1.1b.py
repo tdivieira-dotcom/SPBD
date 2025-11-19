8f8b3785134601c4c32574857f5b0f4d2b4908ef
tdivieira-dotcom <tdi.vieira@campus.fct.unl.pt>
2025-11-19 12:54:56 +0000

    Create Word Count 1.1b.py

---
+++ Lab6/Word Count 1.1b.py
@@ -0,0 +1,21 @@
+from pyspark.sql import *
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+spark = SparkSession.builder.master('local[*]').appName('words').getOrCreate()
+sc = spark.sparkContext
+
+try :
+  lines = sc.textFile('os_maias.txt') \
+            .filter( lambda line : len(line) > 1 ) \
+            .map( lambda line : Row( line = line ) )
+
+  linesDF = spark.createDataFrame( lines )
+  linesDF.createOrReplaceTempView("OSMAIAS")
+
+  x = spark.sql("SELECT count(*) as lines FROM OSMAIAS")
+
+  x.show(5)
+except Exception as err:
+  print(err)
+  sc.stop()
